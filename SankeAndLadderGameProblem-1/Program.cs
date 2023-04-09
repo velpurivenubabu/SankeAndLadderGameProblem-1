2d8bd7ba1e3e3127b0108ba199c924710b47aec5
venubabu <velpurivenubabu123@gmail.com>
2023-04-09 18:44:01 +0530

    [Venubabu] Player Reached Final position

--- SankeAndLadderGameProblem-1/Program.cs
+++ SankeAndLadderGameProblem-1/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace SankeAndLadderGameProblem_1
+﻿using System;
+
+namespace SankeAndLadderGameProblem_1
 {
     internal class Program
     {
@@ -8,33 +10,45 @@
             int initialPosition = 0;
             int finalPosition = 100;
             int playerPosition = initialPosition;
-            Random random = new Random();
-            int DieNumber=random.Next(1,7);
-            Console.WriteLine(DieNumber);
-            //UC3 Setting Options For the Game(NoPlay, Ladd);
-            Random random1 = new Random();
-            int playerOption = random1.Next(0, 3);
-            const int SNAKE = 1;
-            const int LADDER = 2;
-            Console.WriteLine(playerOption);
-            Console.WriteLine(playerPosition);
-            switch (playerOption)
-            {
-                case LADDER:
-                   playerPosition += DieNumber; // If ladder comes player position will be incremented;
-                    
-                    break;
-                case SNAKE:
-                    
-                    playerPosition -= DieNumber; // If Snake comes player position will be decremented;
-                    
-                    break;
-                default:
-                    playerPosition = playerPosition; // If Noplay comes player should stay on the Same position;
-                    break;
-            }
-            Console.WriteLine(playerPosition);   
             Console.WriteLine("Welcome To Snake Ladder Game!");
+            while (playerPosition < finalPosition)
+            {
+                Random random = new Random();
+                int DieNumber = random.Next(1, 7);
+                Console.WriteLine("DieNumber : "+ DieNumber);
+                //UC3 Setting Options For the Game(NoPlay, Ladd);
+                Random random1 = new Random();
+                int playerOption = random1.Next(0, 3);
+                const int SNAKE = 1;
+                const int LADDER = 2;
+                Console.WriteLine("PlayerOption : " + playerOption);
+                //Console.WriteLine("playerPosition : " + playerPosition);
+                switch (playerOption)
+                {
+                    case LADDER:
+                        playerPosition += DieNumber; // If ladder comes player position will be incremented;
+                        if (playerPosition > finalPosition)
+                        {
+                            playerPosition -= DieNumber;
+                        }
+                        break;
+                    case SNAKE:
+                        playerPosition -= DieNumber; // If Snake comes player position will be decremented;
+                        if (playerPosition < initialPosition)
+                        {
+                            playerPosition = initialPosition;
+                        }
+
+                        break;
+                    default:
+                        playerPosition = playerPosition; // If Noplay comes player should stay on the Same position;
+                        break;
+                }
+                Console.WriteLine("playerPosition : " + playerPosition);
+
+            }
+            Console.WriteLine("Player Final Position : " + playerPosition);
+            
         }
     }
 }
