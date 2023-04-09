31c12e9b43124bf573df27cd87625b1a270e3985
venubabu <velpurivenubabu123@gmail.com>
2023-04-09 16:00:44 +0530

    [Venubabu] Initilizing player position

--- SankeAndLadderGameProblem-1/Program.cs
+++ SankeAndLadderGameProblem-1/Program.cs
@@ -4,6 +4,10 @@
     {
         static void Main(string[] args)
         {
+            //UC1 Initilizing initialPosition,finalPosition,playerInitialPosition
+            int initialPosition = 0;
+            int finalPosition = 100;
+            int playerInitialPosition = initialPosition;
             Console.WriteLine("Welcome To Snake Ladder Game!");
         }
     }
