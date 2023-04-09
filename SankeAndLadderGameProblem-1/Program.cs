c9e67efd81bfafff3f95f12d22178fe8a0dd10ff
venubabu <velpurivenubabu123@gmail.com>
2023-04-09 17:16:01 +0530

    [Venubabu] Setting the options for game like Noplay, Snake, Ladder

--- SankeAndLadderGameProblem-1/Program.cs
+++ SankeAndLadderGameProblem-1/Program.cs
@@ -7,10 +7,33 @@
             //UC1 Initilizing initialPosition,finalPosition,playerInitialPosition
             int initialPosition = 0;
             int finalPosition = 100;
-            int playerInitialPosition = initialPosition;
+            int playerPosition = initialPosition;
             Random random = new Random();
             int DieNumber=random.Next(1,7);
             Console.WriteLine(DieNumber);
+            //UC3 Setting Options For the Game(NoPlay, Ladd);
+            Random random1 = new Random();
+            int playerOption = random1.Next(0, 3);
+            const int SNAKE = 1;
+            const int LADDER = 2;
+            Console.WriteLine(playerOption);
+            Console.WriteLine(playerPosition);
+            switch (playerOption)
+            {
+                case LADDER:
+                   playerPosition += DieNumber; // If ladder comes player position will be incremented;
+                    
+                    break;
+                case SNAKE:
+                    
+                    playerPosition -= DieNumber; // If Snake comes player position will be decremented;
+                    
+                    break;
+                default:
+                    playerPosition = playerPosition; // If Noplay comes player should stay on the Same position;
+                    break;
+            }
+            Console.WriteLine(playerPosition);   
             Console.WriteLine("Welcome To Snake Ladder Game!");
         }
     }
